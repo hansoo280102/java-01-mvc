45622f1ff6bac77a2ea4a245d444052422eef59f
hansoo280102 <tantran012080@gmail.com>
2024-12-20 21:57:31 +0700

    45

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,7 +3,13 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+// @SpringBootApplication như bình thường nhưng exlude(trừ) thằng ... ra
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
