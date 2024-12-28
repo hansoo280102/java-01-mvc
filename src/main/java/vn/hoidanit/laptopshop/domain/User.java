7a3a25872e49de1d38534c3322fea303ad1c2d53
hansoo280102 <tantran012080@gmail.com>
2024-12-28 22:18:54 +0700

    78

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -17,6 +19,8 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
 
     public long getId() {
         return id;
@@ -40,6 +44,10 @@ public class User {
 
     public String getPhone() {
         return phone;
+    }
+
+    public String getAvatar() {
+        return avatar;
     }
 
     public void setId(long id) {
@@ -66,9 +74,14 @@ public class User {
         this.phone = phone;
     }
 
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+
 }
